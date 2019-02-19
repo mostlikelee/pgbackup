2ca11d67c50c501afe9704b77198f591f2c54200
Tim Lee <tim@mostlikelee.com>
2019-02-19 14:13:18 -0700

    created unit tests

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,8 +1,12 @@
 import argparse
 
+known_drivers = ['local', 's3']
+
 class DriverAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error("unknown driver")
         namespace.driver = driver.lower()
         namespace.destination = destination
 
@@ -11,9 +15,11 @@ def create_parser():
         Backup PostgreSQL databases locally or to AWS S3
         """)
     parser.add_argument('url',help='URL of database to Backup')
-    parser.add_argument('--driver','-d',
+    parser.add_argument('--driver',
                         help='how and where to store Backup',
                         nargs=2,
+                        # choices=['s3', 'local'],
+                        metavar=("DRIVER", "DESTINATION"),
                         action=DriverAction,
                         required=True)
     return parser
